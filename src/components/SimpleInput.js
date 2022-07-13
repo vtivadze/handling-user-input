89abcef5a1a3fc10e37da43dd39efc50a990fba1
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-13 17:32:37 +0400

    Apply useRef hook for getting user input value

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,34 @@
-const SimpleInput = (props) => {
+import { useRef, useState } from 'react';
+
+const SimpleInput = props => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitionHandler = event => {
+    event.preventDefault();
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue); 
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
